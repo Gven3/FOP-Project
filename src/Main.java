b2fc85b5b5bf51aa038a3f43d2f51400e6eae4eb
Gven3 <giogvenetadze13@gmail.com>
2024-12-28 00:37:59 +0400

    feat: Variable assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder wholeText = new StringBuilder();
+        HashMap<String, Integer> variables = new HashMap<>();
+
+        System.out.println("Enter text (press Ctrl+D or Ctrl+Z to finish):");
+        while (scanner.hasNextLine()) {
+            wholeText.append(scanner.nextLine()).append("\n");
+        }
+
+        String[] splittedCode = wholeText.toString().split("\n");
+
+
+        for (int i = 0; i < splittedCode.length; i++) {
+            String line = splittedCode[i].trim();
+
+            if (line.contains("=")) {
+                String[] assignment = line.split("=", 2);
+                String variableName = assignment[0].trim();
+                String expression = assignment[1].trim();
+
+                variables.put(variableName, Integer.parseInt(expression));
+            }
+
+
+
+        }
+        System.out.println(variables);
     }
 }
